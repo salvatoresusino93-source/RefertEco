c390c01479bd74f1bd87133c94630ed8e04059e4
Salvatore Susino <salvatore.susino93@gmail.com>
2026-06-14 22:58:35 +0200

    Fix firma: aumenta limite body JSON a 50mb (immagini base64)

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const db = require('./database');
 const config = require('./config');
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 // In modalità pkg i file statici stanno accanto all'eseguibile
 const PUBLIC_DIR = process.pkg
